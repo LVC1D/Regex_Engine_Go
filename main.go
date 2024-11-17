bcef0eff1c2e10bf37266d5cad162faf9bf6267c
Illia <54657857+LVC1D@users.noreply.github.com>
2024-11-17 17:58:13 +0200

    Update main.go
    Refactored and passes all - but escape backslash - character checks

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ const (
 	questionMark = "?"
 	asterisk     = "*"
 	plusSign     = "+"
+	wildCard     = "."
+	backSlash    = "\\"
 )
 
 func main() {
@@ -47,49 +49,52 @@ func checkRepetition(regex string, text *string) (r string, isPresent, isOk bool
 	tVal := *text
 
 	switch {
+	// THIS IS SORTED OUT - NO TOUCHY
 	case strings.Contains(regex, questionMark):
 		repCharIndex = strings.Index(regex, questionMark)
 		temp := strings.ReplaceAll(regex, regex[repCharIndex-1:repCharIndex+1], "")
+
 		if regex[repCharIndex-1] == '.' {
-			if *text != temp {
-				r = strings.ReplaceAll(regex, regex[repCharIndex-1:repCharIndex+1], string(tVal[repCharIndex-1]))
-			} else {
-				r = temp
-			}
-			//isPresent, isOk = true, true
-			isPresent, isOk = true, true
-		} else {
-			if strings.Count(*text, string(regex[repCharIndex-1])) == 1 {
+			if tVal != temp {
 				r, isOk = strings.ReplaceAll(regex, questionMark, ""), true
-			} else if !strings.Contains(*text, string(regex[repCharIndex-1])) {
-				r, isOk = temp, true
 			} else {
-				r, isOk = strings.ReplaceAll(regex, questionMark, ""), false
+				r, isOk = temp, true
+			}
+			isPresent = true
+		} else {
+			temp = strings.ReplaceAll(regex, questionMark, "")
+			if strings.Count(*text, string(regex[repCharIndex-1])) == 1 {
+				r, isOk = temp, true
+			} else if !strings.Contains(*text, string(regex[repCharIndex-1])) && len(*text) < len(temp) {
+				r, isOk = strings.ReplaceAll(regex, regex[repCharIndex-1:repCharIndex+1], ""), true
+			} else {
+				r, isOk = temp, false
 			}
 			isPresent = true
 		}
 
+	// THIS ONE AS WELL!!
 	case strings.Contains(regex, asterisk):
 		repCharIndex = strings.Index(regex, asterisk)
 		temp := strings.ReplaceAll(regex, regex[repCharIndex-1:repCharIndex+1], "")
 
 		if regex[repCharIndex-1] == '.' {
 			if *text != temp {
-				r = strings.ReplaceAll(regex, regex[repCharIndex-1:repCharIndex+1], string(tVal[repCharIndex-1]))
-				*text = r
+				r = strings.ReplaceAll(regex, asterisk, "")
 			} else {
 				r = temp
 			}
-			//isPresent, isOk = true, true
 			isOk = true
 		} else {
 			temp = strings.ReplaceAll(regex, asterisk, "")
-			if len(tVal) >= len(temp) && regex[repCharIndex-1] != tVal[repCharIndex-1] {
+			if regex[repCharIndex-1] != tVal[repCharIndex-1] && len(tVal) >= len(temp) {
 				isOk = false
-			} else if len(tVal) < len(temp) && regex[repCharIndex-1] != tVal[repCharIndex-1] {
-				r, isOk = strings.ReplaceAll(regex, regex[repCharIndex-1:repCharIndex+1], ""), true
-			} else {
+			} else if strings.Count(*text, string(regex[repCharIndex-1])) >= 1 {
+				oCount := strings.Count(*text, string(tVal[repCharIndex-1]))
+				temp = strings.ReplaceAll(regex, regex[repCharIndex-1:repCharIndex+1], strings.Repeat(string(tVal[repCharIndex-1]), oCount))
 				r, isOk = temp, true
+			} else {
+				r, isOk = strings.ReplaceAll(regex, regex[repCharIndex-1:repCharIndex+1], ""), true
 			}
 		}
 		isPresent = true
@@ -101,17 +106,17 @@ func checkRepetition(regex string, text *string) (r string, isPresent, isOk bool
 			if *text == temp {
 				isOk = false
 			} else {
-				isOk = true
+				r, isOk = strings.ReplaceAll(regex, plusSign, ""), true
 			}
-			r = temp
 		} else {
 			temp = strings.ReplaceAll(regex, plusSign, "")
-			if strings.Contains(tVal, string(regex[repCharIndex-1])) {
-				isOk = true
+			if strings.Count(tVal, string(regex[repCharIndex-1])) >= 1 {
+				oCount := strings.Count(*text, string(tVal[repCharIndex-1]))
+				temp = strings.ReplaceAll(regex, regex[repCharIndex-1:repCharIndex+1], strings.Repeat(string(tVal[repCharIndex-1]), oCount))
+				r, isOk = temp, true
 			} else {
-				isOk = false
+				r, isOk = temp, false
 			}
-			r = temp
 		}
 		isPresent = true
 
@@ -130,59 +135,35 @@ func checkRegMatch(regex, text string) bool {
 	if len(text) == 0 {
 		return false
 	}
-
-	// this could be part of the function for checking repetition chars
-	if ((regex == ".?" || regex == ".+") && len(text) != 0) || regex == ".*" {
-		return true
-	}
-
 	r, isPresent, isOk := checkRepetition(regex, &text)
 
 	if isPresent && !isOk {
 		return false
 	}
 
-	if !checkFirstChar(r, text) {
+	isFirstOk := checkFirstChar(r, text)
 
-		if strings.HasPrefix(r, prefix) {
+	if strings.HasPrefix(r, prefix) {
+		isFirstOk = checkFirstChar(r[1:], text)
+		if isFirstOk {
 			if strings.HasSuffix(r, suffix) {
-				// r, isPresent, _ = checkRepetition(regex[1:len(regex)-1], &text)
 				if !isPresent {
-					return strings.EqualFold(regex[1:len(regex)-1], text)
-				} else {
-					return /*checkRegMatch(r, text[1:]) */ strings.EqualFold(r[1:len(r)-1], text)
+					return strings.EqualFold(text, r[1:len(r)-1])
 				}
-
+				return checkRegMatch(r[2:len(r)-1], text[1:])
+			} else {
+				return checkRegMatch(r[2:], text[1:])
 			}
-			if checkFirstChar(r[1:], string(text[0])) {
-				r, isPresent, _ = checkRepetition(regex[1:], &text)
-				if !isPresent {
-					return strings.HasPrefix(text, regex[1:])
-				}
-				return checkRegMatch(r[1:], text[1:])
-			}
+		} else {
+			return strings.HasSuffix(text, r)
 		}
-
-		if strings.HasSuffix(r, "$") {
-			// r, _, _ = checkRepetition(regex[:len(regex)-1], text)
-			return strings.HasSuffix(text, r[:len(r)-1])
-		}
-
-		return strings.HasSuffix(text, r)
 	}
 
-	//if the first is OK AND has suffix
-	if strings.HasSuffix(regex, "$") {
-		r, isPresent, _ = checkRepetition(regex[:len(regex)-1], &text)
-		if !isPresent {
-			return checkRegMatch(regex[:len(regex)-1], text[1:]) || strings.HasSuffix(text, regex[:len(regex)-1])
-		}
-		return checkRegMatch(r[1:], text[1:]) || strings.HasSuffix(text, r)
-	} else {
-		if !isPresent && len(r) > len(text) {
-			return false
+	if !strings.HasSuffix(r, suffix) {
+		if !isFirstOk {
+			return strings.HasSuffix(text, r)
 		}
 		return checkRegMatch(r[1:], text[1:])
 	}
-
+	return checkRegMatch(r[1:len(r)-1], text)
 }
